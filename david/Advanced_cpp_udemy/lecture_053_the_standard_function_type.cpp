2b358c46759d2d34d2ca3d33cb1d3e32e1e00a16
David Cohn <DAVIDCO1@rnd-hub.com>
2021-11-21 02:07:45 +0200

    updated code

--- david/Advanced_cpp_udemy/lecture_053_the_standard_function_type.cpp
+++ david/Advanced_cpp_udemy/lecture_053_the_standard_function_type.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <functional>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool check(string& test) {
+    return test.size() == 3;
+}
+
+class Check {
+public:
+    bool operator()(string& test) {
+        return test.size() == 5;
+    }
+} check1;
+
+void run(function<bool(string&)> check) {
+    string test = "dog";
+    cout << check(test) << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int size = 5;
+
+    vector<string> vec{ "one", "two", "three" };
+
+    auto lambda = [size](string test) {return test.size() == size; };
+
+    int count = count_if(vec.begin(), vec.end(), lambda);
+    cout << count << endl;
+
+    count = count_if(vec.begin(), vec.end(), check);
+    cout << count << endl;
+
+    count = count_if(vec.begin(), vec.end(), check1);
+    cout << count << endl;
+
+    run(lambda);
+    run(check1);
+    run(check);
+
+    return 0;
 }
 
